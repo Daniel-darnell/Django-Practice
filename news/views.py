213cc4b26301ed14c5ca89d580acce3790bd20ab
Daniel-darnell <darnelldanny1997@gmail.com>
2020-12-21 14:49:05 +0300

    displayed content on my screen

--- news/views.py
+++ news/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse # Responsible for returning a response to a user.
 
 # Create your views here.
+def welcome(request):
+    return HttpResponse('Welcome to the Kibera Times')
